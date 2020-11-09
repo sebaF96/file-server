634db1ec9f4473fb86f12002ca0ee9a0797e1ca2
sebaf96 <sefernandez96@gmail.com>
2020-11-09 19:55:18 -0300

    Start with json protocol in client-side

--- client/models/Client.py
+++ client/models/Client.py
@@ -1,5 +1,6 @@
 import socket
 import os
+import json
 
 
 class Client:
@@ -8,38 +9,69 @@ class Client:
         self.__socket.connect((address, port))
         print(f"Connected to File Server at {address} on port {port}")
 
-        self.__COMMANDS = {'pwd': self.pwd, 'lpwd': self.lpwd, 'ls': self.ls}
-        self.__COMMANDS_ARGS = {'lcd': self.lcd}
+        self.__REMOTE_COMMANDS = ['pwd', 'cd', 'ls']
+        self.__COMMANDS = {'lpwd': self.lpwd, 'lls': self.lls}
+        self.__COMMANDS_ARGS = {'lcd': self.lcd, 'lls': self.lls}
 
     def run(self):
         while True:
             user_input = input("> ").split()
+            has_argument = len(user_input) == 2
+            command = user_input[0]
+            argument = user_input[1] if has_argument else None
 
-            if user_input[0] not in self.__COMMANDS and user_input[0] not in self.__COMMANDS_ARGS:
+            if command in self.__REMOTE_COMMANDS:
+                self.communicate(command, argument)
+
+            elif command in self.__COMMANDS and argument is None:
+                self.__COMMANDS[command]()
+
+            elif command in self.__COMMANDS_ARGS and argument:
+                self.__COMMANDS_ARGS[command](argument)
+
+            else:
                 print("Command not recognized")
-                continue
 
-            elif user_input[0] in self.__COMMANDS:
-                self.__COMMANDS[user_input[0]]()
+    def communicate(self, command, argument):
+        data = {"command": command, "argument": argument}
+        self.__socket.send(json.dumps(data).encode())
+        response = json.loads(self.__socket.recv(2048).decode())
+        self.show_response(response)
 
-            elif user_input[0] in self.__COMMANDS_ARGS:
-                self.__COMMANDS_ARGS[user_input[0]](user_input[1])
+    @staticmethod
+    def show_response(response: dict):
+        end_color = "\033[0m"
+        if int(response['status_code']) == 200:
+            color = "\033[0;92m"  # Green
+        else:
+            color = "\033[1;31m"  # Red
 
-    @property
-    def socket(self):
-        return self.__socket
+        print(f"{color}{response['status_code']}: {response['status_message']}{end_color}\n")
+        if response['content']:
+            print(response['content'])
 
-    def send(self, content: str):
-        self.__socket.send(content.encode())
-
+    """Local commands"""
     @staticmethod
     def lpwd():
         print(os.getcwd())
 
     @staticmethod
-    def lcd(route):
-        os.chdir(route)
+    def lcd(directory):
+        try:
+            os.chdir(directory)
+        except FileNotFoundError:
+            print("No such directory")
 
+    @staticmethod
+    def lls(directory=None):
+        try:
+            output = os.listdir(directory) if directory else os.listdir()
+            content = None if len(output) == 0 else "\n".join(output)
+            print(content)
+        except FileNotFoundError:
+            print("No such directory")
+
+    """ Remote commands """
     def pwd(self):
         self.send('pwd')
         answer = self.__socket.recv(1024).decode()
