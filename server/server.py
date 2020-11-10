8a2874655beee2bcd5fda4ba2e6d3daa3f9b1266
sebaf96 <sefernandez96@gmail.com>
2020-11-10 20:56:03 -0300

    Finish server getopt and give default ports

--- server/server.py
+++ server/server.py
@@ -5,33 +5,58 @@ import sys
 import socket
 import src
 import multiprocessing
+import threading
+import signal
 
 
-def read_port():
-    (opt, arg) = getopt.getopt(sys.argv[1:], 'p:')
-    if len(opt) != 1:
-        raise getopt.GetoptError("Expected one option [-p] with its argument (port number)")
-
-    port = int(opt[0][1])
-    if port < 1:
-        raise ValueError
-    return port
+def handle_close(s, frame) -> None:
+    print("Closing connections...")
+    sys.exit(0)
 
 
-def attend_client(client_socket, address):
+def read_ports() -> tuple:
+    (opt, arg) = getopt.getopt(sys.argv[1:], 'p:t:', ['port=', 'transfer-port='])
+    port, transfer_port = 8080, 3000
+
+    for (option, argument) in opt:
+        if option == '-p' or option == '--port':
+            port = int(argument)
+        elif option == '-t' or option == '--transfer-port':
+            transfer_port = int(argument)
+
+    if port < 1024 or transfer_port < 1024:
+        raise ConnectionRefusedError("You can't use reserved ports")
+    return port, transfer_port
+
+
+def attend_client(client_socket, address) -> None:
     print('\nGot a connection from', address)
     conn = src.Connection(client_socket, address)
     conn.start()
     print(f"Client {address} disconnected")
 
 
-def main():
+def listen_for_transfers(transfer_socket: socket.socket, local_address: str, transfer_port: int) -> None:
+    print(f"Listening for transfers on port {transfer_port}")
+    while True:
+        transfer_socket.listen(16)
+        client_socket, address = transfer_socket.accept()
+        th = multiprocessing.Process(target=attend_client, args=(client_socket, address))
+        th.start()
+
+
+def main() -> None:
     local_address = socket.gethostbyname(socket.getfqdn() + ".local")
-    port = read_port()
+    main_port, transfer_port = read_ports()
 
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind(('', port))
-    print('Server started at', local_address, 'on port', port)
+    server_socket.bind(('', main_port))
+    transfer_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    transfer_socket.bind(('', transfer_port))
+
+    print(f"Server started at {local_address}")
+    print(f"Listening for connections at port {main_port}")
+    threading.Thread(target=listen_for_transfers, args=(transfer_socket, local_address, transfer_port), daemon=True).start()
     print('Waiting for connections...')
 
     while True:
@@ -42,4 +67,14 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    signal.signal(signal.SIGINT, handle_close)
+    try:
+        main()
+    except getopt.GetoptError as ge:
+        print("Error:", ge)
+    except ConnectionRefusedError as cre:
+        print("Error:", cre)
+    except ValueError:
+        print("Ports must be positive integers")
+    except OSError as oe:
+        print(oe)
