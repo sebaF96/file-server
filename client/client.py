bff03eecabec2d6f107ee0d56c1e552793524f91
sebaf96 <sefernandez96@gmail.com>
2020-11-15 11:43:35 -0300

    Add docstring to client/client.py

--- client/client.py
+++ client/client.py
@@ -6,6 +6,12 @@ import models
 
 
 def read_options() -> tuple:
+    """
+    Reads command-line options looking for address and port number to connect to, in case that one
+    or both options are missing, it will print an error message and exit
+
+    :return: a tuple with two values (the address and the port)
+    """
     address = port = None
     (opt, arg) = getopt.getopt(sys.argv[1:], "a:p:", ["address=", "port="])
 
@@ -26,7 +32,13 @@ def read_options() -> tuple:
     return address, port
 
 
-def main():
+def main() -> None:
+    """
+    Main client function, it will read command-line, create a models.Client instance
+    and call its run() method.
+
+    :return: None
+    """
     address, port = read_options()
     client = models.Client(address, port)
     client.run()
