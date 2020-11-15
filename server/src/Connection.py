c4f627f465f8ef04461b10eb31341cae161367bb
sebaf96 <sefernandez96@gmail.com>
2020-11-15 11:13:04 -0300

    Add docstring to server/src.Connection

--- server/src/Connection.py
+++ server/src/Connection.py
@@ -3,6 +3,12 @@ import json
 
 
 class Connection:
+    """
+    Class representing a connection with a client from the server point of view. An instance of this
+    class is created each time a client connects to the main socket. It will change the current working directory
+    to the one stored on the server's system $HOME environment variable and start receiving commands, executing them,
+    and sending the answer, until the client is disconnected
+    """
     def __init__(self, client_socket, client_address, SESSION_TOKEN, transfers_port):
         self.__client_socket = client_socket
         self.__client_address = client_address
@@ -13,7 +19,13 @@ class Connection:
         self.__COMMANDS = {'pwd': self.pwd, 'ls': self.ls}
         self.__COMMANDS_ARGS = {'cd': self.cd, 'ls': self.ls, 'mkdir': self.mkdir, 'get': self.get, 'put': self.put}
 
-    def start(self):
+    def start(self) -> None:
+        """
+        Connection listen_for_ever method. It will receive commands from the client,
+        execute them and sends answers
+
+        :return: None
+        """
         while True:
             client_datagram = self.__client_socket.recv(2048).decode()
             if not client_datagram:
@@ -35,7 +47,15 @@ class Connection:
             else:
                 self.send_response(500, "Invalid command or argument(s)")
 
-    def send_response(self, status_code, status_message, content=None):
+    def send_response(self, status_code: int, status_message: str, content: str = None) -> None:
+        """
+        Sends a json-formatted answer to the client
+        :param status_code: Integer representing the last command status code
+        :param status_message: Descriptive message associated with the status code
+        :param content: Standard output of the last executed command, if any, None otherwise
+
+        :return: None
+        """
         response = {
             "status_code": status_code,
             "status_message": status_message,
@@ -44,7 +64,17 @@ class Connection:
 
         self.__client_socket.send(json.dumps(response).encode())
 
-    def allow_transfer(self, operation, absolute_path, filesize=None):
+    def allow_transfer(self, operation: str, absolute_path: str, filesize: int = None):
+        """
+        Method called when a transfer request from the client is marked as valid by the server.
+        It will send the transfer's metadata in a json-formatted message to the client
+
+        :param operation: either 'put' or 'get' depending if the client wants to upload or to download a file
+        :param absolute_path: absolute path to the file in server's system
+        :param filesize: if the file is in server's system and client wants to download it, this field
+        will represent the size of that file in bytes
+        :return: None
+        """
         response = {
             "status_code": 200,
             "operation": operation,
@@ -56,11 +86,24 @@ class Connection:
 
         self.__client_socket.send(json.dumps(response).encode())
 
-    def pwd(self):
+    def pwd(self) -> None:
+        """
+        Check for server's current working directory
+        and sends the output to the client in a json-formatted message
+
+        :return: None
+        """
         working_directory = os.getcwd()
         self.send_response(200, "OK", working_directory)
 
     def ls(self, directory=None):
+        """
+        List the given directory or the current directory if none is given,
+        and sends the output to the client in a json-formatted message
+
+        :param directory: String representing the directory to list. None by default
+        :return: None
+        """
         try:
             output = os.listdir(directory) if directory else os.listdir()
             content = None if len(output) == 0 else "\n".join(output)
@@ -69,21 +112,42 @@ class Connection:
         except FileNotFoundError:
             self.send_response(500, "No such directory")
 
-    def cd(self, directory: str):
+    def cd(self, directory: str) -> None:
+        """
+        Change the current working directory to the given one if exists,
+        and sends the output to the client in a json-formatted message
+
+        :param directory: String representing the directory name
+        :return: None
+        """
         try:
             os.chdir(directory)
             self.send_response(200, "OK")
         except FileNotFoundError:
             self.send_response(500, "No such directory")
 
-    def mkdir(self, directory: str):
+    def mkdir(self, directory: str) -> None:
+        """
+        Creates a directory with the given name if that directory doesn't exists,
+        and sends the output to the client in a json-formatted message
+
+        :param directory: Name of the directory to create
+        :return: None
+        """
         try:
             os.mkdir(directory)
             self.send_response(200, "OK")
         except FileExistsError:
             self.send_response(500, "Directory already exists")
 
-    def get(self, filename: str):
+    def get(self, filename: str) -> None:
+        """
+        Handles a get request from the client. Checks if the requested file exists
+        and sends the answer to the client in a json-formatted message
+
+        :param filename: String representing the filename that client is asking for
+        :return: None
+        """
         if not os.path.isfile(filename):
             self.send_response(500, "No such file")
         else:
@@ -91,7 +155,14 @@ class Connection:
             filesize = os.path.getsize(filename)
             self.allow_transfer(operation="get", absolute_path=absolute_path, filesize=filesize)
 
-    def put(self, filename: str):
+    def put(self, filename: str) -> None:
+        """
+        Handles a put request from the client. Checks if the requested file doesn't exists
+        and sends the answer to the client in a json-formatted message
+
+        :param filename: String representing the filename that client wants to upload
+        :return: None
+        """
         if os.path.isfile(filename):
             self.send_response(500, "File already exists")
         else:
