af98d0d5288c00fb94e44e734216b82373913c3a
sebaf96 <sefernandez96@gmail.com>
2020-11-16 10:51:42 -0300

    Add Constant messages to servers main file

--- server/src/server_helper.py
+++ server/src/server_helper.py
@@ -3,6 +3,7 @@ from collections import defaultdict
 
 class Constants:
 
+    # Colors
     COLORS = defaultdict(None)
     COLORS["RED"] = "\033[1;31m"
     COLORS["YELLOW"] = "\033[1;33m"
@@ -10,6 +11,7 @@ class Constants:
     COLORS["GREEN"] = "\033[1;32m"
     COLORS["RESET"] = "\033[0m"
 
+    # Connection
     OK_STATUS_CODE = 200
     ERROR_STATUS_CODE = 500
     OK_MESSAGE = "OK"
@@ -20,14 +22,19 @@ class Constants:
     INVALID_COMMAND = "Invalid command or argument(s)"
     BAD_FORMED_MESSAGE = "Invalid command format, it doesn't respect the protocol"
 
+    # Buffers
     BUFFER_SIZE = 2048
     FILE_BUFFER_SIZE = 4096
+
+    # Main
+    RESERVED_PORTS = "You can't use reserved ports"
+    SERVED_STARTED = "File Server started at"
+    LISTENING_MAIN = "Listening for connections at port"
+    LISTENING_TRANSFERS = "Listening for transfers at port"
+    EXITING = "Closing connections..."
+    PORTS_VALUE_ERROR = "Ports must be positive integers"
+    PORTS_ASSERTION_ERROR = "Main port and Transfer port can not be the same"
 
 
 def print_colored(color: str, message: str):
     print(f"{Constants.COLORS[color.upper()]}{message}{Constants.COLORS['RESET']}")
-
-
-if __name__ == '__main__':
-    print(Constants.OK_MESSAGE)
-    print(Constants.OK_STATUS_CODE)
